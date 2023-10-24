ee13a7aaf01069a4d86e4fbbc4df93c31d9c0e74
LionelMv <mvangicode@gmail.com>
2023-10-25 01:29:38 +0300

    Add 1-fifo_cache file and its test file

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+This module defines a caching class, `FIFOCache`, that is derived
+from the `BaseCaching` class. It provides FIFO caching functionality
+for storing, retrieving and deleting key-value pairs.
+"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    def __init__(self):
+        "Initialize class instance"
+        super().__init__()
+        self.cache_data_list = []
+
+    def put(self, key, item):
+        """
+        Store an item in the cache associated with a given key.
+        Does a FIFO if the number of dictionary items is bigger than MAX_ITEMS.
+        """
+        if key and item:
+            self.cache_data[key] = item
+            self.cache_data_list.append(key)
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                discard = self.cache_data_list.pop(0)
+                self.cache_data.pop(discard)
+                print(f"DISCARD: {discard}")
+
+    def get(self, key):
+        """Retrieve an item from the cache associated with a given key."""
+        if key in self.cache_data:
+            return self.cache_data[key]
+        else:
+            return None
