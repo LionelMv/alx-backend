c892e25172a03ee20b518e5716d7a95e6d181d77
LionelMv <mvangicode@gmail.com>
2023-10-25 00:32:29 +0300

    Add 0-basic_cache file and its test file

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module defines a basic caching class, `BasicCache`, that is derived
+from the `BaseCaching` class. It provides basic caching functionality
+for storing and retrieving key-value pairs.
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Store an item in the cache associated with a given key."""
+    def put(self, key, item):
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieve an item from the cache associated with a given key."""
+        if key in self.cache_data:
+            return self.cache_data[key]
+        else:
+            return None
