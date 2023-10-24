fb2d6d69701c804410cb5c419650b62b98fb2888
LionelMv <mvangicode@gmail.com>
2023-10-25 00:45:53 +0300

    Make file executable, Revert import and remove __init__
    Fix: ALX checker issue

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -4,18 +4,12 @@ This module defines a basic caching class, `BasicCache`, that is derived
 from the `BaseCaching` class. It provides basic caching functionality
 for storing and retrieving key-value pairs.
 """
-from base_caching import BaseCaching
+
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
     """Store an item in the cache associated with a given key."""
-    def __init__(self):
-        """
-        initialize class instance
-        """
-        super().__init__()
-        self.cache_data = {}
-
     def put(self, key, item):
         if key and item:
             self.cache_data[key] = item
