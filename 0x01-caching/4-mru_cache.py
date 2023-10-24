a5ed2ef2ed23903f8831c33ccb9eb8730d059c5d
LionelMv <mvangicode@gmail.com>
2023-10-25 02:41:57 +0300

    Add 4-mru_cache file and its test file

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""
+This module defines a caching class, `MRUCache`, that is derived
+from the `BaseCaching` class. It provides MRU caching functionality
+for storing, retrieving and deleting key-value pairs.
+"""
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """MRUCache Class"""
+    def __init__(self):
+        "Initialize class instance"
+        super().__init__()
+        self.cache_data_list = []
+
+    def put(self, key, item):
+        """
+        Store an item in the cache associated with a given key.
+        Delete items using MRU if the number of dictionary items
+        is bigger than MAX_ITEMS.
+        """
+        if key and item:
+            self.cache_data[key] = item
+            if key not in self.cache_data_list:
+                self.cache_data_list.append(key)
+            else:
+                self.cache_data_list.remove(key)
+                self.cache_data_list.append(key)
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                discard = self.cache_data_list.pop(-2)
+                self.cache_data.pop(discard)
+                print(f"DISCARD: {discard}")
+
+    def get(self, key):
+        """
+        Retrieve an item from the cache associated with a given key.
+        Assist in getting the MRU key in the list.
+        """
+        if key:
+            if key in self.cache_data:
+                self.cache_data_list.remove(key)
+                self.cache_data_list.append(key)
+                return self.cache_data[key]
+            else:
+                return None
