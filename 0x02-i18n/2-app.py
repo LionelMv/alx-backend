13889286b97faf3cedd221aac77ee89605bfac98
LionelMv <mvangicode@gmail.com>
2024-04-12 01:03:33 +0300

    change docs for get_locale fxn

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -20,11 +20,8 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
-    """_summary_
-
-    Returns:
-            _type_: _description_
-    """
+    """Returns the best language translation
+        from a user request."""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
