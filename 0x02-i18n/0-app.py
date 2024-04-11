40e41582b713c7d61f3e52d0b54845724e48bbb1
LionelMv <mvangicode@gmail.com>
2024-04-12 00:30:34 +0300

    add port and host settings

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -11,3 +11,7 @@ app = Flask(__name__)
 def hello_world():
     """Simple hello"""
     return render_template("0-index.html")
+
+
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
