e0ac40794a82e328633f8ed29faff861c8cda1f8
LionelMv <mvangicode@gmail.com>
2024-06-01 14:18:40 +0300

    add 8-job file

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,33 @@
+import { createQueue } from "kue";
+
+const createPushNotificationsJobs = (jobs, queue) => {
+  if (!Array.isArray(jobs)) {
+    throw new Error("Jobs is not an array");
+  }
+
+  jobs.forEach((jobInfo) => {
+    const job = queue.create("push_notification_code_3", jobInfo);
+
+    job
+      .on("enqueue", () => {
+        console.log("Notification job created:", job.id);
+      })
+      .on("complete", () => {
+        console.log(`Notification job ${job.id} completed`);
+      })
+      .on("failed", (error) => {
+        console.log(`Notification job ${job.id} failed: ${error}`);
+      })
+      .on("progress", (progress) => {
+        console.log(`Notification job ${job.id} ${progress}% complete`);
+      });
+
+    job.save((err) => {
+      if (err) {
+        console.log(`Error saving job ${job.id}:`, err);
+      }
+    });
+  });
+};
+
+export default createPushNotificationsJobs;
