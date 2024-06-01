63ae92427c55af2c9a11d9779455854f5231e59e
LionelMv <mvangicode@gmail.com>
2024-06-01 13:53:35 +0300

    add getAsync

--- 0x03-queuing_system_in_js/2-redis_op_async.js
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -1,17 +1,29 @@
 import { createClient, print } from "redis";
+import { promisify } from "util";
 
 const client = createClient();
+
+client.on("connect", () => {
+  console.log("Redis client connected to the server");
+});
 
 client.on("error", (err) => {
   console.log("Redis client not connected to the server:", err.toString());
 });
 
 const setNewSchool = (schoolName, value) => {
-  client.SET(schoolName, value, print);
+  client.set(schoolName, value, print);
 };
 
+const getAsync = promisify(client.get).bind(client);
+
 const displaySchoolValue = async (schoolName) => {
-  console.log(await promisify(client.GET).bind(client)(schoolName));
+  try {
+    const res = await getAsync(schoolName);
+    console.log(res);
+  } catch (error) {
+    console.error("Error retrieving school value:", error);
+  }
 };
 
 async function main() {
@@ -20,7 +32,4 @@ async function main() {
   await displaySchoolValue("HolbertonSanFrancisco");
 }
 
-client.on("connect", async () => {
-  console.log("Redis client connected to the server");
-  await main();
-});
+client.on("connect", main);
